671485bbd229da87b604382a1de88a736bac7d87
Olivier Fuenzalida <OlivierFP@hotmail.com>
2017-12-02 18:15:19 +0100

    Commit

--- Gip Programmeren (2.0)/Gip Programmeren (2.0)/MainWindow.xaml.cs
+++ Gip Programmeren (2.0)/Gip Programmeren (2.0)/MainWindow.xaml.cs
@@ -22,24 +22,165 @@ namespace Gip_Programmeren__2._0_
     /// </summary>
     public partial class MainWindow : Window
     {
-        private String _Connection = String.Format("server=84.196.202.210;user id=Dylan;database=arduino;password=Devaien");
-
         public MainWindow()
         {
             InitializeComponent();
+            OpvullenLeerlingLijst();
+            
         }
 
-        private void OpvullenListBoxen(Leerling _objLeerling)
+        private void OpvullenLeerlingLijst()
         {
-            MySql Aanwezigheid = new MySql(string.Format("SELECT * from aanwezigheidslijst where Leerling_idLeerlingen = {0}", _objLeerling.strIdnummer));
-            Aanwezigheid.Select<Leerling>(lstLeerlinglijst,4);
+            string _conn = string.Format("server=84.196.202.210;user id=Denzel;database=arduino;password={0}", "Denzel");
+            MySqlConnection conn = new MySqlConnection(_conn);
+            conn.Open();
+            string _cmd = string.Format("SELECT * from leerling");
+            MySqlCommand cmd = new MySqlCommand(_cmd, conn);
+            MySqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Leerling objLeerling = new Leerling(dr[0].ToString(), dr[1].ToString(), dr[2].ToString(), Convert.ToInt16(dr[3]), Convert.ToBoolean(dr[4]), Convert.ToBoolean(dr[5]), Convert.ToBoolean(dr[6]), Convert.ToBoolean(dr[7]));
+                lstLeerlinglijst.Items.Add(objLeerling);
+            }
+
+            conn.Close();
+        }
+
+        private void txtZoekNaam_KeyUp(object sender, KeyEventArgs e)
+        {
+            string _conn = string.Format("server=84.196.202.210;user id=Denzel;database=arduino;password={0}", "Denzel");
+            MySqlConnection conn = new MySqlConnection(_conn);
+            conn.Open();
+            string _cmd = string.Format("SELECT * from leerling where LeerlingVNaam like '{0}%' ", txtLeerlingNaam.Text);
+            MySqlCommand cmd = new MySqlCommand(_cmd, conn);
+            MySqlDataReader dr = cmd.ExecuteReader();
+            lstLeerlinglijst.Items.Clear();
+            while (dr.Read())
+            {
+
+                Leerling objLeerling = new Leerling(dr[0].ToString(), dr[1].ToString(), dr[2].ToString(), Convert.ToInt16(dr[3]), Convert.ToBoolean(dr[4]), Convert.ToBoolean(dr[5]), Convert.ToBoolean(dr[6]), Convert.ToBoolean(dr[7]));
+                lstLeerlinglijst.Items.Add(objLeerling);
+            }
+
+            conn.Close();
+        }
+
+        private void OpvullenAanwezigheid(Leerling _objLeerling)
+        {
+            string _conn = string.Format("server=84.196.202.210;user id=Denzel;database=arduino;password={0}", "Denzel");
+            MySqlConnection conn = new MySqlConnection(_conn);
+            conn.Open();
+            string _cmd = string.Format("SELECT * from aanwezigheidslijst where Leerling_idLeerlingen = {0}", _objLeerling.strIdnummer);
+            MySqlCommand cmd = new MySqlCommand(_cmd, conn);
+            MySqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Aanwezigheid objAanwezigheid = new Aanwezigheid(dr[0].ToString(), dr[1].ToString(), dr[2].ToString(), dr[3].ToString());
+                lstAanwezigheidslijst.Items.Add(objAanwezigheid);
+            }
+
+            conn.Close();
         }
 
         private void lstAanwezigheidslijst_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            rbAanwezig.IsChecked = false;
+            rbTeLaat.IsChecked = false;
+            rbTeLaatReden.IsChecked = false;
+            rbAfwezig.IsChecked = false;
+            Aanwezigheid objAanwezigheid = (Aanwezigheid)lstAanwezigheidslijst.SelectedItem;
+            if (objAanwezigheid == null)
+            {
+                return;
+            }
+            switch (objAanwezigheid.strStatusId)
+            {
+                case "1":
+                    rbAanwezig.IsChecked = true;
+                    break;
+
+                case "2":
+                    rbTeLaat.IsChecked = true;
+                    break;
+
+                case "3":
+                    rbTeLaatReden.IsChecked = true;
+                    break;
+
+                case "4":
+                    rbAfwezig.IsChecked = true;
+                    break;
+            }
+        }
+
+        private void UpdateDBStatus(String _CkStatus, Leerling _objLeerling)
+        {
+            string _conn = string.Format("server=84.196.202.210;user id=Denzel;database=arduino;password={0}", "Denzel");
+            MySqlConnection conn = new MySqlConnection(_conn);
+            conn.Open();
+            string _cmd = string.Format("update aanwezigheidslijst set Status_idStatus = {0} where Leerling_idLeerlingen = {1}", _CkStatus, _objLeerling.strIdnummer);
+            MySqlCommand cmd = new MySqlCommand(_cmd, conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    
+        private void lstLeerlinglijst_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
             lstAanwezigheidslijst.Items.Clear();
             Leerling objLeerling = (Leerling)lstLeerlinglijst.SelectedItem;
-            OpvullenListBoxen(objLeerling);
+            OpvullenAanwezigheid(objLeerling);
+        }
+
+        private void rbAanwezig_Checked(object sender, RoutedEventArgs e)
+        {
+            Leerling objLeerling = (Leerling)lstLeerlinglijst.SelectedItem;
+            if (objLeerling == null)
+            {
+                return;
+            }
+            else
+            {
+                UpdateDBStatus("1", objLeerling);
+            }
+        }
+
+        private void rbTeLaatReden_Checked(object sender, RoutedEventArgs e)
+        {
+            Leerling objLeerling = (Leerling)lstLeerlinglijst.SelectedItem;
+            if (objLeerling == null)
+            {
+                return;
+            }
+            else
+            {
+                UpdateDBStatus("3", objLeerling);
+            }
+        }
+
+        private void rbTeLaat_Checked(object sender, RoutedEventArgs e)
+        {
+            Leerling objLeerling = (Leerling)lstLeerlinglijst.SelectedItem;
+            if (objLeerling == null)
+            {
+                return;
+            }
+            else
+            {
+                UpdateDBStatus("2", objLeerling);
+            }
+        }
+
+        private void rbAfwezig_Checked(object sender, RoutedEventArgs e)
+        {
+            Leerling objLeerling = (Leerling)lstLeerlinglijst.SelectedItem;
+            if (objLeerling == null)
+            {
+                return;
+            }
+            else
+            {
+                UpdateDBStatus("4", objLeerling);
+            }
         }
     }
 }
