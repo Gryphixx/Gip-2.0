6b5bbc5b319628c0e7d3f797df2ea12e00d828a0
Olivier Fuenzalida <OlivierFP@hotmail.com>
2018-01-25 15:29:21 +0100

    efsw

---
+++ Gip Programmeren (2.0)/Gip Programmeren (2.0)/Persistence.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gip_Programmeren__2._0_
+{
+    class Persistence
+    {
+    }
+}
