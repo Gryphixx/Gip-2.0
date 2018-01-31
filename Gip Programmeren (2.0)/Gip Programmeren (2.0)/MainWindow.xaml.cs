65c2da4ed0c552201448931ec2cea3f3ad096020
Olivier Fuenzalida <OlivierFP@hotmail.com>
2018-01-31 12:06:25 +0100

    Final Commit

--- Gip Programmeren (2.0)/Gip Programmeren (2.0)/MainWindow.xaml.cs
+++ Gip Programmeren (2.0)/Gip Programmeren (2.0)/MainWindow.xaml.cs
@@ -603,8 +603,6 @@ namespace Gip_Programmeren__2._0_
         private void lstAanwezigheidslijst_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             rbAanwezig.IsChecked = false;
-            rbTeLaat.IsChecked = false;
-            rbTeLaatReden.IsChecked = false;
             rbAfwezig.IsChecked = false;
             Aanwezigheid objAanwezigheid = (Aanwezigheid)lstAanwezigheidslijst.SelectedItem;
             if (objAanwezigheid == null)
@@ -618,14 +616,6 @@ namespace Gip_Programmeren__2._0_
                     break;
 
                 case "2":
-                    rbTeLaat.IsChecked = true;
-                    break;
-
-                case "3":
-                    rbTeLaatReden.IsChecked = true;
-                    break;
-
-                case "4":
                     rbAfwezig.IsChecked = true;
                     break;
             }
